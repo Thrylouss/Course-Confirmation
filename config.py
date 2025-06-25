b7de27b2e15dd9047b8d0b24e1bead18435be9a0
Thrylouss <shoxrux.shorustamov@gmail.com>
2025-06-25 13:43:05 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = '8119127161:AAEHRZLdWhEa_5j8gVQQVi8t02SnPPF8yVE'
+WEBHOOK_URL = 'https://courses-bot.milliybiz.uz/webhook/8119127161:AAEHRZLdWhEa_5j8gVQQVi8t02SnPPF8yVE'
+VERIFY_API_URL = 'http://courses.milliybiz.uz/api/v1/phone-verification/verify_phone/'
